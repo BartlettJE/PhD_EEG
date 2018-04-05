63b5681c4499f002e1a1379d0dfa81efa69b7b0a
James Bartlett <ba_rt_le_tt06@hotmail.co.uk>
2018-04-05 14:52:26 +0100

    Add Commission errors, Omission errors, and Go RT to Kieron's script

--- Analysis_Smoking-NoGo_behavioural.R
+++ Analysis_Smoking-NoGo_behavioural.R
@@ -17,10 +17,34 @@ rawdata <- rawdata %>%
   select(Block:Cue_type, Picture, Stimulus, correct, response, response_time, Subject) %>% #Select only the important variables 
   filter(Block != "Practice") #Remove practice trials 
 
-# How many errors and what was the accuracy? 
+# How many errors and what was the overall accuracy? 
 rawdata %>% 
-  group_by(Subject, Cue_type) %>% 
+  group_by(Subject) %>% 
   summarise(Accuracy = mean(correct) * 100,
             Correct = sum(correct),
             N = n())
+
+# Commision errors - responding to nogo trials
+rawdata %>% 
+  filter(Stimulus == "NoGo") %>% 
+  group_by(Subject) %>% 
+  summarise(correct_resp = sum(correct),
+            N = n(),
+            errors = N - correct_resp)
+
+# Omission errors - missing go trials 
+rawdata %>% 
+  filter(Stimulus == "Go") %>% 
+  group_by(Subject) %>% 
+  summarise(correct_resp = sum(correct),
+            N = n(),
+            errors = N - correct_resp)
+
+# RT to go trials 
+rawdata %>% 
+  filter(Stimulus == "Go" & correct == 1) %>% 
+  group_by(Subject) %>% 
+  summarise(mean_RT = mean(response_time),
+            SD_RT = sd(response_time),
+            N_trials = n())
 
