ebd9728191da2c410105f3dac08966de24079d19
James Bartlett <ba_rt_le_tt06@hotmail.co.uk>
2018-04-05 08:39:53 +0100

    Added commission errors, omission errors, and RT to Go trials

--- Analysis_GoNoGo_behavioural.R
+++ Analysis_GoNoGo_behavioural.R
@@ -19,9 +19,33 @@ rawdata <- rawdata %>%
                             ifelse(Stim_type == "NoGo" & response == "None", 1,
                                    0)))
 
-# How many errors and what was the accuracy? 
+# How many overall errors and what was the accuracy? 
 rawdata %>% 
   group_by(Subject) %>% 
   summarise(Accuracy = mean(corr.resp) * 100,
             Correct = sum(corr.resp),
             Errors = 400 - sum(corr.resp))
+
+# Commision errors - responding to nogo trials
+rawdata %>% 
+  filter(Block != "Practice" & Stim_type == "NoGo") %>% 
+  group_by(Subject) %>% 
+  summarise(correct = sum(corr.resp),
+            N = n(),
+            errors = N - correct)
+
+# Omission errors - missing go trials 
+rawdata %>% 
+  filter(Block != "Practice" & Stim_type == "Go") %>% 
+  group_by(Subject) %>% 
+  summarise(correct = sum(corr.resp),
+            N = n(),
+            errors = N - correct)
+
+# RT to go trials 
+rawdata %>% 
+  filter(Block != "Practice" & Stim_type == "Go" & corr.resp == 1) %>% 
+  group_by(Subject) %>% 
+  summarise(mean_RT = mean(response_time),
+            SD_RT = sd(response_time),
+            N_trials = n())
