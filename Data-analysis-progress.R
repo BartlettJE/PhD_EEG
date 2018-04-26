50b592252b09319498bae77fe1f37a271638e710
James Bartlett <ba_rt_le_tt06@hotmail.co.uk>
2018-04-26 16:53:55 +0100

    Update data collection progress

--- Data-analysis-progress.R
+++ Data-analysis-progress.R
@@ -3,40 +3,40 @@ require(cowplot)
 
 # Create data frame of my progress
 progress <- tribble(~Group, ~Number, ~Target,
-                    "Non-smokers", 4, 18,
+                    "Non-smokers", 14, 18,
                     #"LITS", 0, 18,
                     #"Daily", 0, 18,
                     "Smokers", 1, 36)
 
 # Create data frame of Kieron's progress
 kieron_progress <- tribble(~Group, ~Number, ~Target,
-                    "Non-smokers", 5, 10,
+                    "Non-smokers", 16, 10,
                     #"LITS", 0, 10,
                     #"Daily", 0, 10,
-                    "Smokers", 2, 20)
+                    "Smokers", 3, 20)
 
-# Create list of repeated options - save repetition 
+# Create list of repeated options - save repetition
 base_progress <- list(geom_bar(stat = "identity", colour = "black"),
                       theme(legend.position = "none"),
                       xlab("Group"),
                       facet_wrap(~Group))
 
-# Plot progress of my study 
-progress %>% 
-  ggplot(aes(x = Group, y = Number, fill = Group)) + 
-  base_progress + 
-  expand_limits(y = c(0, 40)) + 
-  scale_y_continuous(breaks = seq(0, 40, by = 5)) + 
-  geom_hline(data = progress, 
-             aes(yintercept = Target), 
+# Plot progress of my study
+progress %>%
+  ggplot(aes(x = Group, y = Number, fill = Group)) +
+  base_progress +
+  expand_limits(y = c(0, 40)) +
+  scale_y_continuous(breaks = seq(0, 40, by = 5)) +
+  geom_hline(data = progress,
+             aes(yintercept = Target),
              linetype = 2)
 
 # Plot progress of Kieron's study
-kieron_progress %>% 
-  ggplot(aes(x = Group, y = Number, fill = Group)) + 
-  base_progress + 
-  expand_limits(y = c(0, 20)) + 
-  scale_y_continuous(breaks = seq(0, 20, by = 2)) + 
-  geom_hline(data = kieron_progress, # Change h line by group 
-             aes(yintercept = Target), 
-             linetype = 2) # dashed line 
+kieron_progress %>%
+  ggplot(aes(x = Group, y = Number, fill = Group)) +
+  base_progress +
+  expand_limits(y = c(0, 20)) +
+  scale_y_continuous(breaks = seq(0, 20, by = 2)) +
+  geom_hline(data = kieron_progress, # Change h line by group
+             aes(yintercept = Target),
+             linetype = 2) # dashed line
