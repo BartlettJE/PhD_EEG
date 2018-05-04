d32481285451f3ee6c4924b81917c78b349427e4
James Bartlett <ba_rt_le_tt06@hotmail.co.uk>
2018-05-04 12:02:10 +0100

    Update grand average R script for batch processing

--- Grand-average-gonogo.R
+++ Grand-average-gonogo.R
@@ -1,6 +1,7 @@
 require(R.matlab) #function to be able to read matlab files - EEG data saved from MNE Pythin 
 require(tidyverse)
 require(pracma) #function to allow the calculation of linear space for plotting 
+require(cowplot)
 
 # Functions to get the average amplitude for each trial type 
 get_go <- function(mat, csv, electrode){
@@ -9,13 +10,13 @@ get_go <- function(mat, csv, electrode){
   # - mat = a matlab file from the saved file in MNE Python
   # - csv = a .csv file from the OpenSesame trial data
   # - electrode - which electrode would you like the data for? 
-  Go_trials <- csv$Stim_type == "Go" #this is only to select Go trials 
+  go_trials <- csv$Stim_type == "Go" & csv$Block != "Practice" #this is only to select correct trials 
   
   dat_elec <- mat[[electrode]] #subset one data frame from the list of data frames 
   # two brackets are used as one returns another list
   # two brackets returns a large array 
-  average_go <- colMeans(dat_elec[1, Go_trials, ]) #create an average for each go trial 
-  return(average_go*1e6) #returns the average (in micro volts) of each go trial along the number of samples per epoch 
+  average_correct <- colMeans(dat_elec[1, go_trials, ]) #create an average for each go trial 
+  return(average_correct*1e6) #returns the average (in micro volts) of each go trial along the number of samples per epoch 
 }
 
 get_nogo <- function(mat, csv, electrode){
@@ -24,44 +25,72 @@ get_nogo <- function(mat, csv, electrode){
   # - mat = a matlab file from the saved file in MNE Python
   # - csv = a .csv file from the OpenSesame trial data
   # - electrode - which electrode would you like the data for? 
-  NoGo_trials <- csv$Stim_type == "NoGo" #this is only to select NoGo trials
+  nogo_trials <- csv$Stim_type== "NoGo" & csv$Block != "Practice" #this is only to select NoGo trials
+  #incorrect_trials <- csv$Block != "Practice"
   
   dat_elec <- mat[[electrode]] #subset one data frame from the list of data frames 
   # two brackets are used as one returns another list
   # two brackets returns a large array 
-  average_nogo <- colMeans(dat_elec[1, NoGo_trials, ]) #Create an average for each NoGo trial 
-  return(average_nogo*1e6) #returns the average (in micro volts) of each nogo trial along the number of samples per epoch 
+  average_incorrect <- colMeans(dat_elec[1, nogo_trials, ]) #Create an average for each NoGo trial 
+  return(average_incorrect*1e6) #returns the average (in micro volts) of each nogo trial along the number of samples per epoch 
 }
 
-electrode = "Pz"
+# prepare batch processing 
 
-dat <- readMat("Clean_data/EEG/Go-NoGo/800_GoNoGo.mat")
-trial_info <- read.csv("Raw_data/Behavioural/Go-NoGo/800-gonogo.csv")
-P_800_go <- get_go(mat = dat, csv = trial_info, electrode = electrode)
-P_800_nogo <- get_nogo(mat = dat, csv = trial_info, electrode = electrode)
+# read a list of .csv (experiment data) to append later 
+csv.files <- list.files(path = "Raw_data/Behavioural/Go-NoGo/",
+                        pattern = "*.csv",
+                        full.names = F)
 
-dat <- readMat("Clean_data/EEG/Go-NoGo/700_GoNoGo.mat")
-trial_info <- read.csv("Raw_data/Behavioural/Go-NoGo/kieron-gonogo.csv")
-P_700_go <- get_go(mat = dat, csv = trial_info, electrode = electrode)
-P_700_nogo <- get_nogo(mat = dat, csv = trial_info, electrode = electrode)
+# read a list of .mat (EEG data) to append later 
+mat.files <- list.files(path = "Rdata/Go-NoGo/",
+                        pattern = "*.mat",
+                        full.names = F)
 
-dat <- readMat("Clean_data/EEG/Go-NoGo/901_GoNoGo.mat")
-trial_info <- read.csv("Raw_data/Behavioural/Go-NoGo/subject-901-gonogo.csv")
-P_901_go <- get_go(mat = dat, csv = trial_info, electrode = electrode)
-P_901_nogo <- get_nogo(mat = dat, csv = trial_info, electrode = electrode)
+# Define which electrode I want to focus on out of the array of 33
+electrode = "Fz"
 
-dat <- readMat("Clean_data/EEG/Go-NoGo/101_GoNoGo.mat")
-trial_info <- read.csv("Raw_data/Behavioural/Go-NoGo/subject-101-gonogo.csv")
-P_101_go <- get_go(mat = dat, csv = trial_info, electrode = electrode)
-P_101_nogo <- get_nogo(mat = dat, csv = trial_info, electrode = electrode)
+# Create two empty matrices to append the voltage at each millisecond 
+go.matrix <- matrix(ncol = 1025)
+nogo.matrix <- matrix(ncol = 1025)
 
-all_go <- rbind(P_101_go, P_700_go, P_800_go, P_901_go)
-all_nogo <- rbind(P_101_nogo, P_700_nogo, P_800_nogo, P_901_nogo)
-grand_go <- colMeans(all_go) 
-grand_nogo <- colMeans(all_nogo)
+# Run a for loop to add the data to each matrix above 
+for (i in 1:length(csv.files)){
+  # for each file, read in the .csv trial information and .mat EEG file
+  trial_info <- read.csv(paste("Raw_data/Behavioural/Go-NoGo/", csv.files[i], sep = "")) 
+  dat <- readMat(paste("Rdata/Go-NoGo/", mat.files[1], sep = ""))
+  
+  # Some defensive coding
+  # Make sure the csv and mat files match up - breaks loop if they do not
+  current.mat <- mat.files[i]
+  current.csv <- csv.files[i]
+  
+  if (substr(current.csv, 0, 4) != substr(current.mat, 0, 4)){
+    print(paste("The files of participant ", substr(current.csv, 0, 4), " do not match.", sep = ""))
+    break
+  }
+  else{ #if all is good, start processing the files
+    # apply functions from above to get erps for correct and incorrect trials
+    go.erp <- get_go(mat = dat, csv = trial_info, electrode = electrode)
+    nogo.erp <- get_nogo(mat = dat, csv = trial_info, electrode = electrode)
+    
+    # append each new matrix row to the previous one 
+    go.matrix <- rbind(go.matrix, go.erp)
+    nogo.matrix <- rbind(nogo.matrix, nogo.erp)
+    
+    # print out the progress and make sure the files match up.
+    print(paste("participant:", substr(csv.files[i], 0, 4), "is complete."))
+  }
+}
 
+# Create a grand average by taking the mean voltage at each measurement point for all participants
+grand_go <- colMeans(go.matrix, na.rm = T) 
+grand_nogo <- colMeans(nogo.matrix, na.rm = T)
+
+# Define the linear space for the x axis of the graphs 
 x = linspace(-200,800,1025)
 
+# Create a plot with both go and nogo waves 
 ggplot() + 
   geom_line(mapping = aes(x = x, y = grand_go), color = "green") + 
   geom_line(mapping = aes(x = x, y = grand_nogo), color = "red") + 
@@ -72,16 +101,12 @@ ggplot() +
   xlab("") + 
   ylab(expression("Mean amplitude"~(mu*"V")))
 
-# grand_diff <- grand_nogo - grand_go
-# lower = grand_diff - std_err(grand_diff) * 1.96 #lower 95% CI 
-# upper = grand_diff + std_err(grand_diff) * 1.96 #upper 95% CI  
-
+# Create a plot with the difference wave
 ggplot() + 
-  geom_line(mapping = aes(x = x, y = grand_diff), color = "red") + 
+  geom_line(mapping = aes(x = x, y = grand_nogo - grand_go), color = "red") + 
   scale_x_discrete(limits = seq(from = -200, to = 800, by = 200)) +
   geom_hline(yintercept = 0, linetype = 2) + 
   geom_vline(xintercept = 0, linetype = 2) + 
-  # geom_ribbon(aes(x = x, ymin = lower, ymax = upper), alpha = 0.5) + #add 95% CI band to plot 
   theme_classic() + 
   annotate("rect", xmin = 175, xmax = 250, ymin = -2, ymax = 3,
            alpha = .5) + 
